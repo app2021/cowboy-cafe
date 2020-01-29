dfa62adbe7c6e37cc1226b180ca71cd64b97b6ec
appino <appino@F1092-20-2108>
2020-01-29 15:05:16 -0600

    backup

---
+++ Data/RustlersRibs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the Ruster's Ribs
+    /// </summary>
+    public class RustlersRibs
+    {
+        /// <summary>
+        /// The price of the entree
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 7.50;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the entree
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 894;
+            }
+
+        }
+
+        // An empty list of the Special Instructions
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+                return instructions;
+            }
+        }
+    }
+}
