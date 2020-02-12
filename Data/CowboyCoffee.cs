86898e38419f6e6a2b4ebb30b1b45e6f6661c7d5
appino <appino@F1092-20-2108>
2020-02-12 15:10:39 -0600

    backup

---
+++ Data/CowboyCoffee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class CowboyCoffee : Drink
+    {
+        public override bool Ice { get; set; } = false;
+
+        public override double Price => throw new NotImplementedException();
+
+        public override uint Calories => throw new NotImplementedException();
+
+        public override List<string> SpecialInstructions => throw new NotImplementedException();
+    }
+}
---
+++ Data/Drink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public abstract class Drink
+    {
+        public virtual Size Size { get; set; }
+
+        public abstract double Price { get; }
+
+
+        public abstract uint Calories { get; }
+
+        public abstract List<string> SpecialInstructions { get; }
+
+        public virtual bool Ice { get; set; } = true;
+
+    }
+}
