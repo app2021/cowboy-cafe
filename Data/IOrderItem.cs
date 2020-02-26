959f251e2a163cf974fdb2207f6809cd743a191d
appino <appino@F1092-20-2108>
2020-02-26 14:48:24 -0600

    backup

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// An interface representing a single item in an order
+    /// </summary>
+    public class IOrderItem
+    {
+        /// <summary>
+        /// The price of this order item
+        /// </summary>
+        double Price { get; }
+
+        /// <summary>
+        /// The special instructions for this IOrderItem
+        /// </summary>
+        List<string> SpecialInstructions { get; }
+    }
+}
