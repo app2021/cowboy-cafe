0e1a264cf6f050b1bb89e6317105054d0fb43f9c
Nick <nickolas.appino@gmail.com>
2020-03-26 14:23:58 -0500

    backup

--- PointOfSale/OrderSummaryControl.xaml.cs
+++ PointOfSale/OrderSummaryControl.xaml.cs
@@ -16,6 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CowboyCafe.Data;
+using CowboyCafe.Extensions;
 
 namespace PointOfSale
 {
@@ -27,6 +29,80 @@ namespace PointOfSale
         public OrderSummaryControl()
         {
             InitializeComponent();
+            
         }
+
+        public void OnRemoveItem(object sender, RoutedEventArgs e)
+        {
+            //  public void Remove(IOrderItem item) {
+
+            var orderControl = this.FindAncestor<OrderControl>();
+            if (orderControl == null) return;
+
+            if (sender is Button button)
+                {
+
+                }
+
+        }
+
+
+        //OrderControl to switch screens, The item that is being customized(the same instance)
+        //The screen that we switch to
+        void itemSelected(object sender, SelectionChangedEventArgs args)
+        {
+
+            var orderControl = this.FindAncestor<OrderControl>();
+            if (orderControl == null) return;
+
+            foreach(object o in args.AddedItems)
+            {
+                if(o is AngryChicken)
+                {
+                    var screen = new CustomizeAngryChicken();
+                    screen.DataContext = o;
+                    orderControl.SwapScreen(screen);
+
+                }
+                else if(o is BakedBeans)
+                {
+                    var screen = new CustomizeSide();
+                    screen.DataContext = o;
+                    orderControl.SwapScreen(screen);
+                }
+                else if (o is ChiliCheeseFries)
+                {
+                    var screen = new CustomizeChiliCheeseFries();
+                    screen.DataContext = o;
+                    orderControl.SwapScreen(screen);
+                }
+                else if (o is CornDodgers)
+                {
+                    var screen = new CustomizeSide();
+                    screen.DataContext = o;
+                    orderControl.SwapScreen(screen);
+                }
+                else if (o is CowboyCoffee)
+                {
+                    var screen = new CustomizeCowboyCoffee();
+                    screen.DataContext = o;
+                    orderControl.SwapScreen(screen);
+                }
+                else if (o is CowpokeChili)
+                {
+                    var screen = new CustomizeCowpokeChili();
+                    screen.DataContext = o;
+                    orderControl.SwapScreen(screen);
+                }
+                else if (o is DakotaDoubleBurger)
+                {
+                    var screen = new CustomizeDakotaDoubleBurger();
+                    screen.DataContext = o;
+                    orderControl.SwapScreen(screen);
+                }
+            }
+
+        }
+
     }
 }
